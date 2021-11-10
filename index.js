9d772f2bc0d56e3d85af65d41a90347e5f8082df
Jake Colledge <jakecolledgework@gmail.com>
2021-11-10 21:38:47 +1030

    added classes to lib folder and tested prompting in index

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Employee = require('./lib/Employee');
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
-    }
-  ])
-  .then((response) =>
-    console.log(response.username)
-  );
+const createEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the Employees name?',
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'What is the employees ID',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: 'What is the employees email?',
+            name: 'email'
+        }
+    ])
+}
+
+const createManager = () => {
+    createEmployee()
+    .then((answers) => {
+        const manager = new Employee(answers.name, answers.id, answers.email)
+        console.log(manager)
+    });
+}
+
+createManager()
+
